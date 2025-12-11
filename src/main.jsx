20ec0f0d5d4b1f158402392c269b0eaf00d9099b
Vishal Gurung <goblin01@g0>
2025-12-11 12:23:31 +0900

    add a snow effect for winter only

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,23 @@ import Portfolio from './App';
 import './index.css';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/react';
+import Snowfall from 'react-snowfall';
+
+const isWinterSeason = () => {
+  const month = new Date().getMonth() + 1;
+  return month === 12 || month === 1 || month === 2;
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+      {/* Snow effect runs globally and only in winter*/}
+      {isWinterSeason() && (
+        <Snowfall 
+        //snowflakeCount={200}
+        />
+      )}
+      
       <Portfolio />
       <Analytics />
       <SpeedInsights />
