0776b29b5fd95c6bf7a4709cc0f27510c0a15aae
Vishal Gurung <168546557+g0GobliN@users.noreply.github.com>
2025-12-15 06:31:11 +0000

    felt unnecessary so removing it

--- src/main.jsx
+++ src/main.jsx
@@ -7,20 +7,20 @@ import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/react';
 import Snowfall from 'react-snowfall';
 
-const isWinterSeason = () => {
-  const month = new Date().getMonth() + 1;
-  return month === 12 || month === 1 || month === 2;
-};
+// const isWinterSeason = () => {
+//   const month = new Date().getMonth() + 1;
+//   return month === 12 || month === 1 || month === 2;
+// };
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      {/* Snow effect runs globally and only in winter*/}
+      {/* Snow effect runs globally and only in winter
       {isWinterSeason() && (
         <Snowfall 
         //snowflakeCount={200}
         />
-      )}
+      )} */}
       
       <Portfolio />
       <Analytics />
